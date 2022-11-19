d65879244a69f8561d19e6617829dfbf710870a2
Dante <danteaugusto.10@gmail.com>
2022-11-19 11:20:49 -0300

    lendo as entradas

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        // Para leitura
+        Scanner sc = new Scanner(System.in);
+        int n, d;
+        n = sc.nextInt();
+        d = sc.nextInt();
+        int edges[][] = new int[n][n];
+        int x = 0;
+        for(int i = x; i < n-1; i++){
+            x++;
+            for(int j = i+1; j < n; j++){
+                edges[i][j] = sc.nextInt();
+            }
+        }
+        /*
+        Com isso, o exemplo:
+        5 2
+        5 10 15 2
+        21 2 45
+        53 12
+        13
+        Será armazenado na matriz edge da seguinte forma:
+           c1 c2 c3 c4 c5
+        c1  0  5 10 15  2
+        c2  0  0 21  2 45
+        c3  0  0  0 53 12
+        c4  0  0  0  0 13
+        c5  0  0  0  0  0
+         */
     }
 }
