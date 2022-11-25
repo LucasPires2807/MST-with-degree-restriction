18c751466f7d6910bf641aaca4993cfa685cf103
Lucas Pires <lucaspires2807@gmail.com>
2022-11-25 00:08:30 -0300

    Startet isBitsetSpanningTree

--- src/ds/Graph.java
+++ src/ds/Graph.java
@@ -2,6 +2,7 @@ package ds;
 
 import java.util.Map;
 import java.util.Vector;
+import java.util.BitSet;
 import java.util.HashMap;
 
 /*
@@ -63,7 +64,7 @@ public class Graph {
     // Usar para todas as combinações possíveis
 
     public Graph kruskal(){
-        Graph a = new Graph();
+        Graph g = new Graph();
         DisjointSet<Character> ds = new DisjointSet<Character>();
         Map<Character, DisjointSet<Character>> m = new HashMap<Character, DisjointSet<Character>>();
 
@@ -78,14 +79,60 @@ public class Graph {
             DisjointSet<Character> first = m.get(e.origin);
             DisjointSet<Character> second = m.get(e.destination);
             if(ds.find(first) != ds.find(second)){
-                a.nodes.add(e.origin);
-                a.nodes.add(e.destination);
-                a.connectVertices(e);
+                g.nodes.add(e.origin);
+                g.nodes.add(e.destination);
+                g.connectVertices(e);
                 first.union(second);
             }
         }
 
-        return a;
+        return g;
+    }
+
+    public boolean isBitsetSpanningTree(BitSet b){
+        Graph g = new Graph();
+        // Converting bitset to graph;
+        for(int i = 0; i < b.size(); ++i){
+            if(b.get(i) == true){    // edge in the graph
+                edge e = bitToEdge(b, i);
+                g.connectVertices(e);
+            }
+        }
+
+        return !hasCycle(g);
+    }
+
+    private boolean hasCycle(Graph g){
+        // Rodar dfs
+        return true;
+    }
+
+    /*
+     * This method the logic works as follows:
+     * As an exemple, we have a complete graph with the edges A, B, C and D.
+     * Each edge at the vector of edges is at the following order:
+     *      (A,B), (A,C), (A,D), (B,C), (B,D), (C,D)
+     * Note that there's always a subrange that the letter, from A to the previous of the last letter,
+     * is the first at the pair at the subrange.
+     * 
+     * The method starts by taking the last pair, witch is always (previous of the last letter, last letter),
+     * that can be denoted as [upperBound - count, upperbound] closed interval, with upperBound = b.size()-1 = 5
+     * and count = 0.
+     * 
+     * At the condition of the while loop, it checks wethear the given index is at the current range.
+     * If it isn't, the boundaries are tightened by incrementing the count and decreasing the upperBound by count
+     * so that we have now the next subrange.
+     * If it is, does a convertion in a proper way and then returns the edge.
+     */
+    private edge bitToEdge(BitSet b, int idx){
+        int upperBound = b.size()-1; // A variable to check if the given i is in a determined range
+        int count = 0;               // Auxiliary variable to help throught the algorithm
+        while(upperBound - count <= idx && idx <= upperBound){
+            upperBound -= ++count;
+        }
+        char origin = (char) (65 + nodes.size() - (count + 1));
+        char destination = (char) ((int)origin + (idx - upperBound + count ) + 1);
+        return new edge(origin,destination,0); // Necessário ajeitar o weight
     }
 
     public class edge{
